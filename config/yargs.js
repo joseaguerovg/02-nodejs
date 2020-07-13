f0e099a7fb12107c96f9eee2553fc779da037d88
José Aguero <jaguero@moldeable.com>
2020-07-12 21:12:20 -0400

    Initial commit

---
+++ config/yargs.js
@@ -0,0 +1,29 @@
+const descripcion = {
+    alias: 'd',
+    demand: true,
+    desc: 'Descripción de la tarea por hacer'
+};
+
+const completado = {
+    alias: 'c',
+    default: true,
+    desc: 'Marca como completado o pendiente la tarea'
+}
+
+const argv = require('yargs')
+    .command('crear', 'Crea una tarea por hacer', {
+        descripcion
+    })
+    .command('actualizar', 'Actualiza el estado completado de una tarea', {
+        descripcion,
+        completado
+    })
+    .command('borrar', 'Eliminar una tarea por hacer', {
+        descripcion
+    })
+    .help()
+    .argv;
+
+module.exports = {
+    argv
+}
